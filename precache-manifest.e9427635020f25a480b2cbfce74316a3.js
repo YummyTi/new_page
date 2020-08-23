96ba3746c50c97ebd137e8007c1e28912dc06aa6
Umrzoq Toshkentov <utoshkentov@gmail.com>
2020-08-23 19:54:36 +0500

    Updates

---
+++ precache-manifest.e9427635020f25a480b2cbfce74316a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c096172c960618b154d14c9ef2e199",
+    "url": "/new_page/index.html"
+  },
+  {
+    "revision": "f8cd36802f58995c861d",
+    "url": "/new_page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "62972efcbc516f1bd214",
+    "url": "/new_page/static/js/2.10b73e00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new_page/static/js/2.10b73e00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8cd36802f58995c861d",
+    "url": "/new_page/static/js/main.fd530a3f.chunk.js"
+  },
+  {
+    "revision": "1f30cfe22dd5deb0b01c",
+    "url": "/new_page/static/js/runtime-main.4b675caf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new_page/static/media/logo.5d5d9eef.svg"
+  }
+]);
